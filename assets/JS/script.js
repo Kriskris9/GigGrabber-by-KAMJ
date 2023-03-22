4b424656a74d6cb8209265e826a679109d3fcd41
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-03-22 11:20:42 -0700

    added a getArtist and getConcerts function to javascript

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,28 +1,36 @@
-var id = "edc48f414ecacffb0e5d6e0406e465b6";
 var search = document.querySelector(".input");
-var button = document.querySelector("button")
+var button = document.querySelector("button");
+var id = "edc48f414ecacffb0e5d6e0406e465b6";
 var input;
 
-function searchArtist(event) {
-    event.preventDefault();
-
-    var input =  search.value.trim()
-    if (input.includes(" ")) {
-        input = input.replace(" ", "%20")
-        console.log(input);
-    }
-    else {
-        console.log(input)
-    }
-    fetch(`https://rest.bandsintown.com/artists/${input}/?app_id=${id}`)
-
+function getArtist(input) {
+  fetch(`https://rest.bandsintown.com/artists/${input}/?app_id=${id}`)
     .then(function (response) {
-        return response.json();
+      return response.json();
     })
     .then(function (data) {
-        console.log(data);
+      console.log(data);
     });
 }
-
+function getConcerts(input) {
+  fetch(`https://rest.bandsintown.com/artists/${input}/events?app_id=${id}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+function searchArtist(event) {
+  event.preventDefault();
+  input = search.value.trim();
+  if (input.includes(" ")) {
+    input = input.replace(" ", "%20");
+    console.log(input);
+  } else {
+    console.log(input);
+  }
+  getArtist(input);
+  getConcerts(input);
+}
 button.addEventListener("click", searchArtist);
-
