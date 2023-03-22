66dfce576d801f9bfb92a5f9c39c8f1117e86845
alexanderscher <alexcscher@gmail.com>
2023-03-22 10:07:20 -0700

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,9 @@
-apiKey = "e903f7c8560c58a90ec3791a2fff9dd7";
-console.log(apiKey);
+var id = "edc48f414ecacffb0e5d6e0406e465b6";
 
-var myHeaders = new Headers();
-myHeaders.append("x-rapidapi-key", apiKey);
-myHeaders.append("x-rapidapi-host", "v1.formula-1.api-sports.io");
-
-var requestOptions = {
-  method: "GET",
-  headers: myHeaders,
-  redirect: "follow",
-};
-
-function getTrack() {
-  fetch("https://v1.formula-1.api-sports.io/competitions", requestOptions)
-    .then((response) => response.text())
-    .then(function (result) {
-      var data = JSON.parse(result);
-      console.log(data.response);
-    })
-
-    .catch((error) => console.log("error", error));
-}
-
-getTrack();
-
-// https://api-sports.io/documentation/formula-1/v1#section/Sample-Scripts/Swift
+fetch(` https://rest.bandsintown.com/artists/Justin%20Bieber/?app_id=${id}`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
