d588e579767d45771e1646dbca7ac73422a8cd1e
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-03-22 12:05:52 -0700

    updatd script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,8 @@
 var search = document.querySelector(".input");
 var button = document.querySelector("button");
+var bitResults = document.querySelector(".results-bandsintown");
 var id = "edc48f414ecacffb0e5d6e0406e465b6";
 var input;
-var bitResults = document.querySelector("results-bandsintown")
 
 function getArtist(input) {
   fetch(`https://rest.bandsintown.com/artists/${input}/?app_id=${id}`)
@@ -13,6 +13,7 @@ function getArtist(input) {
       console.log(data);
     });
 }
+
 function getConcerts(input) {
   fetch(`https://rest.bandsintown.com/artists/${input}/events?app_id=${id}`)
     .then(function (response) {
@@ -20,25 +21,25 @@ function getConcerts(input) {
     })
     .then(function (data) {
       console.log(data);
-      data.forEach(function(c){
-      
-var card = document.createElement('article')
-var infoVenue = document.createElement('p')
-var infoDate= document.createElement('p')
+      data.forEach(function (c) {
+        var card = document.createElement("article");
+        card.className = "card";
+        var infoVenue = document.createElement("p");
+        var infoDate = document.createElement("p");
 
+        var venue = c.venue.name;
+        var date = c.datetime;
 
-  var venue = c.venue.name;
-        var date= c.datetime;
+        infoVenue.innerHTML = venue;
+        infoDate.innerHTML = date;
 
-infoVenue.innerHTML = venue;
-infoDate.innerHTML = date;
-
-card.append(infoVenue);
-card.append(infoDate);
-bitResults.append(card);
-      })
+        card.append(infoVenue);
+        card.append(infoDate);
+        bitResults.append(card);
+      });
     });
 }
+
 function searchArtist(event) {
   event.preventDefault();
   input = search.value.trim();
@@ -48,7 +49,8 @@ function searchArtist(event) {
   } else {
     console.log(input);
   }
-  getArtist(input);
+  //   getArtist(input);
   getConcerts(input);
 }
+
 button.addEventListener("click", searchArtist);
