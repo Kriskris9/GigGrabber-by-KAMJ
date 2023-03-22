c6866df7d463710c9e06cd12d2e9bae288398b45
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-03-22 12:01:38 -0700

    updated script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,6 +2,7 @@ var search = document.querySelector(".input");
 var button = document.querySelector("button");
 var id = "edc48f414ecacffb0e5d6e0406e465b6";
 var input;
+var bitResults = document.querySelector("results-bandsintown")
 
 function getArtist(input) {
   fetch(`https://rest.bandsintown.com/artists/${input}/?app_id=${id}`)
@@ -19,6 +20,23 @@ function getConcerts(input) {
     })
     .then(function (data) {
       console.log(data);
+      data.forEach(function(c){
+      
+var card = document.createElement('article')
+var infoVenue = document.createElement('p')
+var infoDate= document.createElement('p')
+
+
+  var venue = c.venue.name;
+        var date= c.datetime;
+
+infoVenue.innerHTML = venue;
+infoDate.innerHTML = date;
+
+card.append(infoVenue);
+card.append(infoDate);
+bitResults.append(card);
+      })
     });
 }
 function searchArtist(event) {
