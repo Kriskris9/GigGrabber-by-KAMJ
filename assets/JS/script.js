bacb62c1b231451e778a43fe9e6a839cd8ab4ee4
alexanderscher <alexcscher@gmail.com>
2023-03-22 19:57:58 -0700

    a bunch of css and js work

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,19 +1,21 @@
 var search = document.querySelector(".input");
 var button = document.querySelector("button");
-var bitResults = document.querySelector(".results-bandsintown");
+var bitResults = document.querySelector(".card-container");
 var topArtist = document.querySelector("#top-artist");
+var head = document.querySelector(".header-bandsintown");
 var id = "edc48f414ecacffb0e5d6e0406e465b6";
 var input;
+var actualInput;
 
-function getArtist(input) {
-  fetch(`https://rest.bandsintown.com/artists/${input}/?app_id=${id}`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-}
+// function getArtist(input) {
+//   fetch(`https://rest.bandsintown.com/artists/${input}/?app_id=${id}`)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// }
 
 function getConcerts(input) {
   fetch(`https://rest.bandsintown.com/artists/${input}/events?app_id=${id}`)
@@ -22,88 +24,102 @@ function getConcerts(input) {
     })
     .then(function (data) {
       console.log(data);
-
-      venues = [];
-      dates = [];
-      city = [];
-      state = [];
-
-      var concertInfo = [];
       bitResults.innerHTML = "";
+      head.innerHTML = "";
+      if (Object.keys(data).length === 0) {
+        console.log("yes");
+        var empty = document.createElement("h3");
+        empty.innerHTML = "No search results for " + actualInput;
+        head.append(empty);
 
-      data.forEach(function (c) {
-        var card = document.createElement("article");
-        card.className = "card";
-        var infoVenue = document.createElement("p");
-        var infoDate = document.createElement("p");
-        var infoCity = document.createElement("p");
-        var infoState = document.createElement("p");
-
-        var venue = c.venue.name;
-        var date = c.datetime;
-        var city = c.venue.city;
-        var state = c.venue.region;
-
-        obj = {
-          venueStorage: venue,
-          dateStorage: date,
-          cityStorage: city,
-          stateStorage: state,
+        concertInfo = {
+          isEmpty: "empty",
+          artistName: actualInput,
         };
+        console.log(concertInfo);
+        localStorage.setItem("concertInfo", JSON.stringify(concertInfo));
+      } else {
+        var showing = document.createElement("h3");
+        showing.innerHTML = "Showing concert results for " + actualInput;
+        head.append(showing);
+        var concertInfo = [];
 
-        concertInfo.push(obj);
+        data.forEach(function (c) {
+          var card = document.createElement("div");
+          card.className = "card";
+          var infoVenue = document.createElement("p");
+          var infoDate = document.createElement("p");
+          var infoCity = document.createElement("p");
 
-        infoVenue.innerHTML = venue;
-        infoDate.innerHTML = date;
-        infoCity.innerHTML = city;
-        infoState.innerHTML = state;
+          var venue = c.venue.name;
+          var date = c.datetime;
+          var city = c.venue.city;
+          var state = c.venue.region;
 
-        card.append(infoVenue);
-        card.append(infoDate);
-        card.append(infoCity);
-        card.append(infoState);
-        bitResults.append(card);
-      });
+          obj = {
+            artistName: actualInput,
+            venueStorage: venue,
+            dateStorage: date,
+            cityStorage: `${city}, ${state}`,
+          };
 
-      localStorage.setItem("concertInfo", JSON.stringify(concertInfo));
+          concertInfo.push(obj);
+
+          infoVenue.innerHTML = venue;
+          infoDate.innerHTML = date;
+          infoCity.innerHTML = `${city}, ${state}`;
+
+          card.append(infoVenue);
+          card.append(infoDate);
+          card.append(infoCity);
+          bitResults.append(card);
+        });
+
+        localStorage.setItem("concertInfo", JSON.stringify(concertInfo));
+      }
     });
 }
 
 function getStorage() {
-  var lastSearch = JSON.parse(localStorage.getItem("concertInfo"));
+  lastSearch = JSON.parse(localStorage.getItem("concertInfo"));
 
-  lastSearch.forEach(function (s) {
-    var card = document.createElement("article");
-    card.className = "card";
-    var infoVenue = document.createElement("p");
-    var infoDate = document.createElement("p");
-    var infoCity = document.createElement("p");
-    var infoState = document.createElement("p");
+  if (lastSearch.isEmpty) {
+    var empty = document.createElement("h3");
+    empty.innerHTML = "No search results for " + lastSearch.artistName;
+    head.append(empty);
+  } else {
+    var showing = document.createElement("h3");
+    showing.innerHTML =
+      "Showing concert results for " + lastSearch[0].artistName;
+    head.append(showing);
 
-    infoVenue.innerHTML = s.venueStorage;
-    infoDate.innerHTML = s.dateStorage;
-    infoCity.innerHTML = s.cityStorage;
-    infoState.innerHTML = s.stateStorage;
+    lastSearch.forEach(function (s) {
+      var card = document.createElement("div");
+      card.className = "card";
+      var infoVenue = document.createElement("p");
+      var infoDate = document.createElement("p");
+      var infoCity = document.createElement("p");
 
-    card.append(infoVenue);
-    card.append(infoDate);
-    card.append(infoCity);
-    card.append(infoState);
-    bitResults.append(card);
-  });
+      infoVenue.innerHTML = s.venueStorage;
+      infoDate.innerHTML = s.dateStorage;
+      infoCity.innerHTML = s.cityStorage;
+
+      card.append(infoVenue);
+      card.append(infoDate);
+      card.append(infoCity);
+      bitResults.append(card);
+    });
+  }
 }
 
 function searchArtist(event) {
   event.preventDefault();
-
+  actualInput = search.value;
   input = search.value.trim();
   if (input.includes(" ")) {
     input = input.replace(" ", "%20");
-    console.log(input);
-  } else {
-    console.log(input);
   }
-  getConcerts(input);
+  getConcerts(input, actualInput);
 }
 
 button.addEventListener("click", searchArtist);
